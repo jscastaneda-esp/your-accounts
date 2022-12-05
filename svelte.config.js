18fbecbbd88dd88a0823092a9affcecd04f0f191
Jonathan Castañeda <jscastaneda@modusoftware.com>
2022-12-05 15:54:31 -0500

    Ajuste para despliegue en Vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,6 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-auto';
+//import adapter from '@sveltejs/adapter-node';
+//import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,9 +12,7 @@ const config = {
 	}),
 
 	kit: {
-		adapter: adapter({
-			precompress: true
-		})
+		adapter: adapter()
 	}
 };
 
