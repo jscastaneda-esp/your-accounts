295d4d771d7de9a576f4e45a7dccbb319048faf6
Jonathan Castañeda <jcaatanedaesp@gmail.com>
2023-06-06 16:30:08 -0500

    feat: Se agrega configuración inicial para proteger rutas de tRPC

--- src/lib/trpc/context.ts
+++ src/lib/trpc/context.ts
@@ -1,7 +1,22 @@
 import type { inferAsyncReturnType } from '@trpc/server'
+import { JWT_SECRET } from '$env/static/private'
+import type { RequestEvent } from '@sveltejs/kit'
+import jwt from 'jsonwebtoken'
 
-export async function createContext() {
-	return {}
+export async function createContext({ cookies }: RequestEvent) {
+	console.log(cookies.getAll())
+
+	try {
+		const token = cookies.get('jwt')
+		// 👆 or, if we're using HTTP headers based authentication, we could do something like this:
+		// const token = event.request.headers.get('authorization')?.replace('Bearer ', '');
+
+		const { id: userId } = jwt.verify(token || '', JWT_SECRET) as { id: string }
+
+		return { userId }
+	} catch {
+		return { userId: '' }
+	}
 }
 
 export type Context = inferAsyncReturnType<typeof createContext>
--- src/routes/(app)/budget/[id]/+page.server.ts
+++ src/routes/(app)/budget/[id]/+page.server.ts
@@ -3,4 +3,4 @@ import { router } from '$lib/trpc/router'
 import { createContext } from '$lib/trpc/context'
 
 export const load: PageServerLoad = async (event) =>
-	router.createCaller(await createContext()).budgets.getById(Number(event.params.id))
+	router.createCaller(await createContext(event)).budgets.getById(Number(event.params.id))
