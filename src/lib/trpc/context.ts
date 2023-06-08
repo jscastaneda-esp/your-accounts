43c0f2b44638ebeda0cfe6a87c8c025c9515d4b9
Jonathan Castañeda <jcaatanedaesp@gmail.com>
2023-06-07 19:16:51 -0500

    fix: Se corrige error de importación del modulo 'jsonwebtoken'

--- src/lib/trpc/context.ts
+++ src/lib/trpc/context.ts
@@ -1,12 +1,12 @@
 import type { inferAsyncReturnType } from '@trpc/server'
 import { JWT_SECRET } from '$env/static/private'
 import type { RequestEvent } from '@sveltejs/kit'
-import { verify } from 'jsonwebtoken'
+import jwt from 'jsonwebtoken'
 
 export async function createContext({ cookies }: RequestEvent) {
 	try {
 		const token = cookies.get('jwt')
-		const { tokenApi } = verify(token || '', JWT_SECRET) as { tokenApi: string }
+		const { tokenApi } = jwt.verify(token || '', JWT_SECRET) as { tokenApi: string }
 		return { tokenApi }
 	} catch {
 		return { tokenApi: '' }
--- src/routes/login/+server.ts
+++ src/routes/login/+server.ts
@@ -1,11 +1,11 @@
 import type { RequestHandler } from './$types'
-import { sign } from 'jsonwebtoken'
+import jwt from 'jsonwebtoken'
 import { JWT_SECRET } from '$env/static/private'
 
 export const POST = (async ({ cookies, request }) => {
 	try {
 		const { uuid, email } = await request.json()
-		const token = sign({ uuid, email, tokenApi: `<token>${uuid}` }, JWT_SECRET)
+		const token = jwt.sign({ uuid, email, tokenApi: `<token>${uuid}` }, JWT_SECRET)
 		cookies.set('jwt', token, { path: '/', secure: false })
 		return new Response()
 	} catch (err) {
