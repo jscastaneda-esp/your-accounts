c3994b747cc6daca3617051df79d90363f4d35a2
Jonathan Castañeda <jcaatanedaesp@gmail.com>
2023-06-16 20:01:33 -0500

    fix: Se solucionan errores relacionados con la autenticación
    1. Se asignan parámetros específicos a la cookie que almacena el token
    2. Se controla que exista un token y que sea valido para visitar las páginas protegidas

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,5 +2,32 @@ import { createContext } from '$lib/trpc/context'
 import { router } from '$lib/trpc/router'
 import type { Handle } from '@sveltejs/kit'
 import { createTRPCHandle } from 'trpc-sveltekit'
+import jwt from 'jsonwebtoken'
+import { JWT_SECRET } from '$env/static/private'
 
-export const handle: Handle = createTRPCHandle({ router, createContext })
+function redirectLogin() {
+	return new Response(undefined, {
+		status: 303,
+		headers: { location: '/login' }
+	})
+}
+
+const fnTRPCHandle = createTRPCHandle({ router, createContext })
+export const handle: Handle = async ({ event, resolve }) => {
+	if (!event.url.pathname.startsWith('/trpc') && !event.url.pathname.startsWith('/login')) {
+		const token = event.cookies.get('jwt')
+
+		try {
+			if (!token) {
+				throw new Error('Token not found')
+			} else {
+				jwt.verify(token, JWT_SECRET)
+			}
+		} catch (e: unknown) {
+			console.log(event.url.pathname, e)
+			return redirectLogin()
+		}
+	}
+
+	return await fnTRPCHandle({ event, resolve })
+}
--- src/routes/login/+server.ts
+++ src/routes/login/+server.ts
@@ -1,12 +1,21 @@
 import type { RequestHandler } from './$types'
 import jwt from 'jsonwebtoken'
 import { JWT_SECRET } from '$env/static/private'
+import { dev } from '$app/environment'
 
 export const POST = (async ({ cookies, request }) => {
 	try {
 		const { uuid, email } = await request.json()
+
 		const token = jwt.sign({ uuid, email, tokenApi: `<token>${uuid}` }, JWT_SECRET)
-		cookies.set('jwt', token, { path: '/', secure: false })
+		cookies.set('jwt', token, {
+			path: '/',
+			httpOnly: true,
+			sameSite: 'strict',
+			secure: !dev,
+			maxAge: 60 * 60 * 24 * 30
+		})
+
 		return new Response()
 	} catch (err) {
 		console.log(err)
